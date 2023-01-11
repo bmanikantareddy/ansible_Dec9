f6865ca16498fc5c0fd8997a3aea88f830967f25
bmanikantareddy <bmaniknatareddy@gmail.com>
2023-01-11 11:28:24 +0000

    added log rotation script

---
+++ ansible-role-fluentbit/files/log_rotation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# log rotation script for Osqueryd and fluent-bit
+
+log_rotation()
+{
+    FILE=$1
+    FILE_BAK="$1.bak"
+    if [ -f "${FILE_BAK}" ]; then
+        rm -rf ${FILE_BAK}
+    fi
+
+    if [ -f "${FILE}" ]; then
+        mv $FILE $FILE_BAK
+        echo > $FILE
+    fi
+}
+
+# OSqueryd log rotation
+log_rotation "/var/log/osquery/osqueryd.results.log"
+# Fluent-bit log rotation
+#log_rotation "replace with fluent-bit path"
+
+
+
